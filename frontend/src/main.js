cfb3b59fc0742cabb0d4ed696f118f8134ca628b
xingyu <capriccio_xy@sina.com>
2023-02-22 19:36:12 +0800

    前端登录初步

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,19 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+ 
+import ElementUI from 'element-ui';
+import Element from 'element-ui'
+Vue.use(ElementUI);
+Vue.use(Element);
+import './assets/styles/element-variables.scss'
+
+import '@/assets/styles/index.scss' // global css
+
+import '@/assets/icons' // icon
+
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
